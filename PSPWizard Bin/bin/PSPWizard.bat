5cc65fc5d28070b6bab73075f87d429a3b2fcfa5
franksn90 <s.frankausowen@googlemail.com>
2022-08-22 10:24:12 +0200

    Reduce project to Java source

--- PSPWizard Bin/bin/PSPWizard.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-@echo launching PSPWizard ...
-
-set BASEDIR=%~dp0
-set LIBS=%BASEDIR%lib\AbsolutLayout.jar:%BASEDIR%lib\swing-layout-1.0.4.jar
-
-java -classpath "%LIBS%" -jar "%BASEDIR%PSPWizard.jar"
