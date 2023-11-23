8a0abee9d28e9a5e35e016585753cb083fec6280
Marvin Taube <marvin.taube@pigeonport.de>
2023-11-23 07:04:33 +0100

    implemented basic Universality pattern creation from request without constraints

--- restAPI/src/main/java/restapi/MainController.java
+++ restAPI/src/main/java/restapi/MainController.java
@@ -3,16 +3,25 @@ package restapi;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import psp.sel.patterns.Pattern;
 
 @RestController
 public class MainController {
 
+    private final RequestService requestService;
+
     Logger logger = LoggerFactory.getLogger(MainController.class);
+
+    @Autowired
+    public MainController(RequestService requestService) {
+        this.requestService = requestService;
+    }
 
     @PostMapping("/transformPattern")
     public ResponseEntity<ResponsePayload> handleDashboardRequest(@RequestBody RequestPayload payload){
@@ -20,10 +29,19 @@ public class MainController {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
         try {
-            //TODO Handle Request
             ObjectMapper objectMapper = new ObjectMapper();
             logger.info(objectMapper.writeValueAsString(payload));
-            return new ResponseEntity<>(new ResponsePayload("Test SEG", "TestLogic"),HttpStatus.OK);
+            Pattern pattern = null;
+            try {
+                pattern = requestService.createPatternFromRequest(payload);
+                logger.info("Pattern created: " + pattern.toString());
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+            return new ResponseEntity<>(new ResponsePayload(
+                    "SEG Holder",
+                    "Request Login Holder"
+            ),HttpStatus.OK);
         } catch (Exception exception){
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
--- restAPI/src/main/java/restapi/RequestService.java
+++ restAPI/src/main/java/restapi/RequestService.java
@@ -1,4 +1,29 @@
 package restapi;
 
+import org.springframework.stereotype.Service;
+import psp.sel.EventImpl;
+import psp.sel.patterns.Pattern;
+import psp.sel.patterns.occurrence.Universality;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Service
 public class RequestService {
+
+    public Pattern createPatternFromRequest(RequestPayload payload){
+
+        HashMap<String, EventImpl> eventList = new HashMap<>();
+
+        for (EventPayload eventPayload : payload.getEvents()) {
+            eventList.put(eventPayload.name, new EventImpl(eventPayload.name, eventPayload.specification));
+        }
+
+        switch (payload.getPattern()){
+            case "Universality":
+                return new Universality(eventList.get(payload.getPatternProps().get("event").asText()), null, null);
+            default: return null;
+        }
+    }
 }
